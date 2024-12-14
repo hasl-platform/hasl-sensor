fc033784554c4d8402e063078f44abcaefdda77e
Mikalai Kukhta <mikalai.kukhta@gmail.com>
2024-12-14 23:53:38 +0100

    optimized departure sensors

--- custom_components/hasl3/sensors/__init__.py
+++ custom_components/hasl3/sensors/__init__.py
@@ -1,7 +1,6 @@
 """Module for the sensors component."""
 
 from .departure import (
-    DEPARTURE_SENSORS,
     DepartureDataUpdateCoordinator,
-    TrafikDepartureSensor,
+    DeparturesSensor,
 )
